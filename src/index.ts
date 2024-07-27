dd370569f18dbdb2c9ed6b4cdfd3760e6e7a3cc2
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-07-27 16:26:49 +0530

    adding database credentials

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Client } from 'pg'
+ 
+const client = new Client({
+  host: 'pg-10540c55-bsingharnav-37d6.g.aivencloud.com',
+  port: 22524,
+  database: 'defaultdb',
+  user: 'avnadmin',
+  password: 'AVNS_LaJ6KdHERlxEUMUHjGm',
+})
+
+client.connect()
+
+async function createUsersTable(){
+
+}
