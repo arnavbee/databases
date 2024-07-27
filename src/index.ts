e0347f051c8f94f67428f357c0c231d747ac4697
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-07-27 17:10:14 +0530

    insert

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
-import { Client } from 'pg'
- 
-const client = new Client({
-  host: 'pg-10540c55-bsingharnav-37d6.g.aivencloud.com',
-  port: 22524,
-  database: 'defaultdb',
-  user: 'avnadmin',
-  password: 'AVNS_LaJ6KdHERlxEUMUHjGm',
-})
+import { Client } from 'pg';
 
-client.connect()
+async function insertData() {
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',
+    user: 'postgres',
+    password: 'mysecretpassword',
+  });
 
-async function createUsersTable(){
-    
-
+  try {
+    await client.connect(); // Ensure client connection is established
+    const insertQuery = "INSERT INTO users (username, email, password) VALUES ('username2', 'user3@example.com', 'user_password');";
+    const res = await client.query(insertQuery);
+    console.log('Insertion success:', res); // Output insertion result
+  } catch (err) {
+    console.error('Error during the insertion:', err);
+  } finally {
+    await client.end(); // Close the client connection
+  }
 }
+
+insertData();
